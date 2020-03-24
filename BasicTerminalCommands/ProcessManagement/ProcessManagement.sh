9ac24692e1742fb9ce83f2d997902b9aaf41945b
Tushar-W <twankhede95@gmail.com>
2020-03-24 19:13:23 +0530

    ADDED: process management commands..

---
+++ BasicTerminalCommands/ProcessManagement/ProcessManagement.sh
@@ -0,0 +1,33 @@
+
+ 
+ pwd
+ cd ..
+ cd ..
+ cd Tushar/
+ ls
+ cd Developement/
+ ls
+ mkdir ProcessManagement
+ ls
+ cd ProcessManagement/
+ nano -T 3 HelloWorld.java
+ javac HelloWorld.java
+ java HelloWorld
+ ls -l
+ java HelloWorld &
+ ps -elf | more
+ ps -elf
+ ps -elf | grep java | grep -v grep
+ jobs
+ fg %1
+ jobs
+ bg %1
+ jobs
+ killall java
+ ls
+ nano -T 3 HelloWorld1.java
+ javac HelloWorld1.java
+ java HelloWorld1
+ ^c
+ nano ProcessManagement.sh
+
