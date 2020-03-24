a02044c22c9d0ac55dd73a2cbe084d6262e5ffd0
Tushar-W <twankhede95@gmail.com>
2020-03-24 19:07:23 +0530

    ADDED: file management commands..

---
+++ BasicTerminalCommands/FileManagement/CommandsOfFilesManagement/FileManagement.sh
@@ -0,0 +1,38 @@
+ pwd
+ cd ..
+ ls
+ mkdir FileManagement
+ ls
+ cd FileManagement
+ mkdir temp
+ ls
+ rm -R temp
+ ls
+ mkdir -p temp/temp
+ ls
+ cd temp/
+ ls
+ touch test
+ ls -p
+ cp test test1
+ ls -p
+ mv test1 test2
+ ls -p
+ ln -s test temp/
+ ls -p temp/
+ ls
+ ln -s ../linux-content/linux_chit_sheet.pdf ~/QuickLink/
+ ls -l ~/QuickLink/
+ cd ~p
+ cd ~
+ pwd
+ mkdir QuickLink
+ ls
+ cd Tushar/Developement/FileManagement/temp/
+ ln -s ../linux-content/linux_chit_sheet.pdf ~/QuickLink/
+ ls -l ~/QuickLink/
+ whereis ls
+ ls /bin
+ man ls
+ nano FileManagement.sh
+
