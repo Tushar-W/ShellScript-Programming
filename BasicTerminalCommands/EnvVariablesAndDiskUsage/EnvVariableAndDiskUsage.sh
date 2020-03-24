9b93d81481ea4390fa6483866dc7ee73f3c7235e
Tushar-W <twankhede95@gmail.com>
2020-03-24 19:18:15 +0530

    ADDED: environment variables and disk usage commands..

---
+++ BasicTerminalCommands/EnvVariablesAndDiskUsage/EnvVariableAndDiskUsage.sh
@@ -0,0 +1,40 @@
+ 
+  cd ..
+  EnvVariablesAndDiskUsage
+  mkdir EnvVariablesAndDiskUsage
+  cd EnvVariablesAndDiskUsage/
+  env
+  env | grep -i user
+  echo $USER
+  echo $SHELL
+  echo $HOME
+  du -sh linux-content/
+  cd ..
+  cd TerminalCommands/
+  du -sh linux-content/
+  ls
+  du -sm * | sort -nr
+  cd linux-content
+  du -sm * | sort -nr
+  du -sk * | sort -n
+  cd ~/Developement
+  cd ..
+  cd Developement/
+  touch commands.txt
+  cd ..
+  TerminalCommands/linux-content/
+  find ~/Developement -name commands.txt
+  cd TerminalCommands/linux-content/
+  find ~/Tushar/Developement -name commands.txt
+  cd ..
+  cd Developement
+  ls
+  cd File
+  cd FileManagement/
+  ls
+  cd temp
+  ls
+  find . -type f -empty
+  history
+  history > EnvVariableAndDiskUsage.sh
+  nano EnvVariableAndDiskUsage.sh
