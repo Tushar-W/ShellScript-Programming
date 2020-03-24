4053f68cbd18d796089256aef27609ed871555c2
Tushar-W <twankhede95@gmail.com>
2020-03-24 18:55:12 +0530

    ADDED: file and directory commands..

---
+++ BasicTerminalCommands/FileAndDirectoryCommands/FileAndDirectoryCommands.sh
@@ -0,0 +1,14 @@
+cd ..
+ls
+mkdir FileAndDirectoryCommands
+ls
+cd FileAndDirectoryCommands/
+ls
+nano FileAndDirectoryCommands.sh
+pwd
+ls .
+ls ..
+ls ~
+ls -a
+ls -l
+nano FileAndDirectoryCommands.sh
