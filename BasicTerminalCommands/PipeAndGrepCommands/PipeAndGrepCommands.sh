a4bf8097d7ea0c8ff7120830bee50623a983952e
Tushar-W <twankhede95@gmail.com>
2020-03-24 19:11:06 +0530

    ADDED: pipe and grep commands..

---
+++ BasicTerminalCommands/PipeAndGrepCommands/PipeAndGrepCommands.sh
@@ -0,0 +1,22 @@
+ pwd
+ cd ..
+ mkdir PipeAndGrepCommands
+ cd ..
+ cd TerminalCommands/
+ find . -size +5M
+ find . -name *.pdf -size +5M
+ find . -size +5M |grep pdf
+ find . -name data*
+ grep -i captain linux-content/data.csv
+ grep -r -i captain
+ cd ..
+ ls
+ cd Developement/
+ ls
+ cd FileManagement/
+ ls
+ cd temp
+ ls -p | grep /
+ ls -p | grep -v /
+ nano PipeAndGrepCommands.sh
+
