bce1bf56bc8199466d90faaaab48cb40565e022b
Tushar-W <twankhede95@gmail.com>
2020-03-24 20:13:57 +0530

    ADDED: commands on programming concept..

---
+++ ProgrammingConcept/CommandsOfProgrammingConcepts/ProgrammingConcept.sh
@@ -0,0 +1,71 @@
+
+ pwd
+ cd ..
+ ls
+ mkdir ProgrammingConcept
+  mkdir ProgrammingConstruct
+  ls
+  cd ProgrammingConcept/
+  ls
+  mkdir sequence
+  cd sequence
+  nano addition.sh
+  ./addition.sh
+  nano addition.sh
+  ./addition.sh
+  nano addition.sh
+  ./addition.sh
+  nano subtraction.sh
+  ./subtraction.sh
+  ./addition.sh
+  nano addition.sh
+  ./addition.sh
+  nano subtraction.sh
+  nano addition.sh
+  ./addition.sh
+  nano addition.sh
+  ./addition.sh
+  nano multiplication.sh
+  cp subtraction.sh multiplication.sh
+  nano multiplication.sh
+  ./multiplication.sh
+  cp multiplication.sh division.sh
+  nano division.sh
+  ./division.sh
+  nano division.sh
+  cp division.sh modulus.sh
+  nano modulus.sh
+  ./modulus.sh
+  cp subtraction.sh additionusingTakingInputFromUser.sh
+  cp subtraction.sh additionUsingTakingInputFromUser.sh
+  nano additionUsingTakingInputFromUser.sh
+  ./additionUsingTakingInputFromUser.sh
+  nano subtractionUsingUserInput.sh
+  ./additionUsingTakingInputFromUser.sh
+  cp additionUsingTakingInputFromUser.sh subtractionUsingUserInput.sh
+  nano subtractionUsingUserInput.sh
+  ./subtractionUsingUserInput.sh
+  cp additionUsingTakingInputFromUser.sh multiplicationUsingUserInput.sh
+  nano multiplicationUsingUserInput.sh
+  ./multiplicationUsingUserInput.sh
+  cp additionUsingTakingInputFromUser.sh divisionUsingUserInput.sh
+  nano divisionUsingUserInput.sh
+  ./divisionUsingUserInput.sh
+  cp additionUsingTakingInputFromUser.sh modulusUsingUserInput.sh
+  nano modulusUsingUserInput.sh
+  ./modulusUsingUserInput.sh
+  nano ProgramForEchoHelloUser.sh
+  ./ProgramForEchoHelloUser.sh
+  nano HelloUser.sh
+  ./HelloUser.sh
+  nano programForArithmeticOperations.sh
+  ./programForArithmeticOperations.sh
+  nano programForArithmeticOperations.sh
+  ./programForArithmeticOperations.sh
+  nano programForArithmeticOperations.sh
+  ./programForArithmeticOperations.sh
+  nano programForArithmeticOperations.sh
+  ./programForArithmeticOperations.sh
+  nano programForArithmeticOperations.sh
+  ./programForArithmeticOperations.sh
+  nano ProgrammingConcept.sh
