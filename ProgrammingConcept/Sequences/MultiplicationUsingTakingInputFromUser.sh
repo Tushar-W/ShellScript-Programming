f757d13d0e73629971287768c7fd53049d4451ac
Tushar-W <twankhede95@gmail.com>
2020-03-24 23:04:00 +0530

    ADDED:multiplication of two numbers by taking user input..

---
+++ ProgrammingConcept/Sequences/MultiplicationUsingTakingInputFromUser.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash -x
+
+read -p "Enter First Number : " firstNum
+read -p "Enter Second Number : " secondNum
+
+multiplication=$(( $x * $y ));
+
+echo "Multiplication Is : " $multiplication
