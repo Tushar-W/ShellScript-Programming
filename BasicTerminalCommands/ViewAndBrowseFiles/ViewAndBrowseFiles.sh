a0325f1a820fc382e7a2bae44693e14f68b27912
Tushar-W <twankhede95@gmail.com>
2020-03-24 19:05:37 +0530

    ADDED: view and browse files commands..

---
+++ BasicTerminalCommands/ViewAndBrowseFiles/ViewAndBrowseFiles.sh
@@ -0,0 +1,21 @@
+ cd ..
+ mkdir ViewAndBrowseFiles
+ ls
+ cd ViewAndBrowseFiles/
+ ls -l etc/passwd
+ ls -l passwd
+ ls
+ cd ViewAndBrowseFiles/
+ mkdir temp
+ cd temp
+ mkdir etc
+ ls -l /etc/passwd
+ ls -l /etc
+ less /etc/passwd
+ head /etc/passwd
+ head /etc/passwd-
+ tail -2 /etc/passwd
+ cat /etc/passwd
+ cd..
+ cd ..
+ nano ViewAndBrowseFiles.sh
