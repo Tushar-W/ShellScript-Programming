11e8f72d15b23d8582c05eb1eb18bd0082e17f1c
Tushar-W <twankhede95@gmail.com>
2020-03-24 18:40:48 +0530

    ADDED: terminal commands..

---
+++ BasicTerminalCommands/TerminalCommands/TerminalCommands.sh
@@ -0,0 +1,23 @@
+
+pwd
+cd ..
+cd admin1/
+mkdir Tushar
+cd Tushar/
+mkdir TerminalCommands
+cd TerminalCommands/
+java -version
+git --version
+git clone https://github.com/edurekavivekh/linux-content.git
+ls
+cd linux-content/
+ls
+cd ..
+cd ..
+mkdir Developement
+ls
+cd Developement/
+mkdir TerminalCommands
+cd TerminalCommands/
+ls
+nano TerminalCommands.sh
