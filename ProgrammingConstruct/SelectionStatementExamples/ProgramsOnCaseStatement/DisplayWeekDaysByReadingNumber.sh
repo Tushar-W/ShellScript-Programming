9005a4e5588d62caf1cf236b8a3e6ae60e76246f
Tushar-W <twankhede95@gmail.com>
2020-03-25 10:43:02 +0530

    ADDED:program on display week days by reading number..

---
+++ ProgrammingConstruct/SelectionStatementExamples/ProgramsOnCaseStatement/DisplayWeekDaysByReadingNumber.sh
@@ -0,0 +1,31 @@
+
+#!/bin/bash -x
+
+read -p  "Enter Week Day Number   :" day;
+
+case	$day	in
+			1)
+				echo  "SUNDAY"
+				;;
+			2)
+				echo  "MONDAY"
+				;;
+			3)
+				echo  "TUESDAY"
+				;;
+			4)
+				echo  "WEDNESDAY"
+				;;
+			5)
+				echo  "THURSDAY"
+				;;
+			6)
+				echo  "FRIDAY"
+				;;
+			7)
+				echo  "SATURDAY"
+				;;
+			*)
+         	echo  "Please Enter Single Digit Number"
+				;;
+esac
