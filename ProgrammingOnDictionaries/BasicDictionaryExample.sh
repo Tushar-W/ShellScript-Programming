f0a4b83ee000f0fec1ab6dfadcea783f7d9c6e8e
Tushar-W <twankhede95@gmail.com>
2020-03-30 19:48:58 +0530

    MODIFIED:remove extra spaces

--- ProgrammingOnDictionaries/BasicDictionaryExample.sh
+++ ProgrammingOnDictionaries/BasicDictionaryExample.sh
@@ -1,14 +1,14 @@
-#!/bin/bash	-x
+#!/bin/bash -x
 #Note using Latest Bash Version 5.0
 
-declare	-A	sounds
-sounds[dog]="bark"
-sounds[cow]="moo"
-sounds[bird]="tweet"
-sounds[wolf]="howl"
+declare -A sounds
+sounds[dog]="bark";
+sounds[cow]="moo";
+sounds[bird]="tweet";
+sounds[wolf]="howl";
 
-echo	"Dog Sound "	${sounds[dog]}	#Dog's Sounds
-echo	"All Animal Sound	"	${sounds[@]}	#All Animal Sounds(values)
-echo	"Animal "	${!sounds[@]}	#All Animal(keys)
-echo	"Number Of Animals "	${#sounds[@]}	#Number Of Elements
-unset	sounds[dog]	#delete dog
+echo "Dog Sound" ${sounds[dog]}				#Dog's Sounds
+echo "All Animal Sound" ${sounds[@]}		#All Animal Sounds(values)
+echo "Animal" ${!sounds[@]}					#All Animal(keys)
+echo "Number Of Animals" ${#sounds[@]}		#Number Of Elements
+unset sounds[dog]									#delete dog
