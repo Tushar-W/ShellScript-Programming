9fbb61351ef76082e8ae13bb8c3b222f6adb1ff1
Tushar-W <62276718+Tushar-W@users.noreply.github.com>
2020-03-30 19:41:52 +0530

    MODIFIED: comments indentation

--- ProgrammingOnDictionaries/BasicDictionaryExample.sh
+++ ProgrammingOnDictionaries/BasicDictionaryExample.sh
@@ -7,8 +7,8 @@ sounds[cow]="moo"
 sounds[bird]="tweet"
 sounds[wolf]="howl"
 
-echo	"Dog Sound "	${sounds[dog]}	#Dog's Sounds
-echo	"All Animal Sound	"	${sounds[@]}	#All Animal Sounds(values)
-echo	"Animal "	${!sounds[@]}	#All Animal(keys)
-echo	"Number Of Animals "	${#sounds[@]}	#Number Of Elements
-unset	sounds[dog]	#delete dog
+echo	"Dog Sound "	${sounds[dog]}	        #Dog's Sounds
+echo	"All Animal Sound	"	${sounds[@]}	    #All Animal Sounds(values)
+echo	"Animal "	${!sounds[@]}             	#All Animal(keys)
+echo	"Number Of Animals "	${#sounds[@]}	  #Number Of Elements
+unset	sounds[dog]                         	#delete dog
