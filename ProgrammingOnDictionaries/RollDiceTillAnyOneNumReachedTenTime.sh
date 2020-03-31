f70c9cfd5616e8a0b86716ca8c34a81950e54e39
Tushar-W <twankhede95@gmail.com>
2020-03-31 23:18:00 +0530

    MODIFIED: add fuctions for getting max and min time dice num

--- ProgrammingOnDictionaries/RollDiceTillAnyOneNumReachedTenTime.sh
+++ ProgrammingOnDictionaries/RollDiceTillAnyOneNumReachedTenTime.sh
@@ -1,14 +1,58 @@
 #!/bin/bash -x
 
 declare -A diceNum
-# VARIABLES
-count=1;
+diceNum[1]=0;
+diceNum[2]=0;
+diceNum[3]=0;
+diceNum[4]=0;
+diceNum[5]=0;
+diceNum[6]=0;
 
-while [ $count != 10 ]
+function getMaxTimeRollDiceNum() {
+      diceNum=$@;
+      for ((count=1; count<=6; count++))
+      do
+         if [ ${diceNum[$count]} -eq 10 ];
+         then
+               echo $count
+               break;
+         fi
+      done
+}
+function getMinTimeRollDiceNum() {
+      diceNum=$@;
+      local minNum=$1
+      for ((count=1; count<=6; count++))
+      do
+         if [ ${diceNum[$count]} -lt $minNum ];
+         then
+               minNum=${diceNum[$count]};
+         fi
+      done
+      local count=1;
+      while [ $count != 7 ]
+      do
+         if [ ${diceNum[$count]} -eq $minNum ];
+         then
+               echo $count
+         fi
+         ((count++))
+      done
+}
+
+while [ true ]
 do
-	diceNum[$count]=$((RANDOM%6 + 1))
-	((count++))
+   result=$((RANDOM%6 + 1))
+   num=${diceNum[$result]}
+   echo $num
+   diceNum[$result]=$((++num))
+   if [ $num -eq 10 ];
+   then
+         break
+   fi
 done
-
+maxTimeRollDiceNum=$( getMaxTimeRollDiceNum ${diceNum[@]} );
+minTimeRollDiceNum=$( getMinTimeRollDiceNum ${diceNum[@]} ${diceNum[1]} );
 echo ${diceNum[@]}
 echo ${!diceNum[@]}
+
